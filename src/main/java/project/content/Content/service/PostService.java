7794e4f2a24210e497fa6b2e0c4c7054bb18ddcb
shldhll <iam.shldhll@gmail.com>
2021-10-02 17:38:50 +0530

    added post service

---
+++ src/main/java/project/content/Content/service/PostService.java
@@ -0,0 +1,38 @@
+package project.content.Content.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.content.Content.model.Post;
+import project.content.Content.repository.PostRepository;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    void addPost(Post post) {
+        repository.save(post);
+    }
+
+    Post getPostById(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    List<Post> getPosts() {
+        return repository.findAll();
+    }
+
+    void updatePost(Post post) {
+        Post existingPost = getPostById(post.getId());
+        existingPost.setTitle(post.getTitle());
+        existingPost.setAuthors(post.getAuthors());
+        existingPost.setText(post.getText());
+        repository.save(post);
+    }
+
+    void deleteById(Integer id) {
+        repository.deleteById(id);
+    }
+}
